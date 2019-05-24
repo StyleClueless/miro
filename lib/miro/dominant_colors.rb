28e8acffeb868891475b26b7fb89163a6bea065c
Ivan Verevkin <ivan.p.verevkin@gmail.com>
2019-05-24 11:03:43 +0200

    added -channel RGBA

--- lib/miro/dominant_colors.rb
+++ lib/miro/dominant_colors.rb
@@ -114,9 +114,9 @@ module Miro
     def image_magick_params
       pallete = Rails.root.join("public", "palette.gif")
       if Miro.histogram?
-        "':in[0]' -resize :resolution -type palette -depth 2 -colors 64 -colors :colors -colorspace :quantize -quantize :quantize -alpha remove -format %c histogram:info:"
+        "':in[0]' -resize :resolution -type palette -depth 2 -colors 64 -channel RGBA -colorspace :quantize -quantize :quantize -alpha remove -format %c histogram:info:"
       else
-        "':in[0]' -resize :resolution -type palette -depth 2 -colors 64 -colors :colors -colorspace :quantize -quantize :quantize :out"
+        "':in[0]' -resize :resolution -type palette -depth 2 -colors 64 -channel RGBA -colorspace :quantize -quantize :quantize :out"
       end
     end
 
